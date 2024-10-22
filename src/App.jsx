e9847e320837522e9b5b1437779ce8cf24e40cce
elia-nu <elias.nuruad@gmail.com>
2024-10-22 14:03:48 +0300

    h

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'leaflet/dist/leaflet.css';
 import Home from './components/clientside/home'
 import Navbar from './components/commen/navbar'
 import Footer from './components/commen/footer'
