050317379a9bf793f53d2050251c5195bbdc9303
elia-nu <elias.nuruad@gmail.com>
2024-10-22 11:28:31 +0300

    s

--- src/components/commen/navbar.jsx
+++ src/components/commen/navbar.jsx
@@ -10,13 +10,13 @@ const Navbar = () => {
       <div className="container mx-auto px-4 py-4">
         <nav className="flex justify-between items-center">
           <Link to="/" className="text-3xl font-bold">
-            <span className="text-teal-700">Car</span>
-            <span className="text-teal-300">Zu</span>
+            <span className="text-red-700">Car</span>
+            <span className="text-red-300">Zu</span>
           </Link>
           
           {/* Hamburger menu for mobile */}
           <button
-            className="lg:hidden text-teal-700 focus:outline-none"
+            className="lg:hidden text-red-700 focus:outline-none"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -25,7 +25,7 @@ const Navbar = () => {
           </button>
 
           {/* Desktop menu */}
-          <div className="hidden lg:flex space-x-6 text-teal-700">
+          <div className="hidden lg:flex space-x-6 text-red-700">
             <NavLink to="/rent-your-car">Rent Your Car</NavLink>
             <NavLink to="/help">Help</NavLink>
             <NavLink to="/signup">Sign Up</NavLink>
@@ -51,7 +51,7 @@ const NavLink = ({ to, children, mobile }) => (
   <Link
     to={to}
     className={`font-medium transition duration-300 ease-in-out
-      ${mobile ? 'block py-2 hover:bg-red-600 px-4 rounded' : 'hover:text-teal-300'}`}
+      ${mobile ? 'block py-2 hover:bg-red-600 px-4 rounded' : 'hover:text-red-300'}`}
   >
     {children}
   </Link>
